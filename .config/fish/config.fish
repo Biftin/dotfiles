0b9407a70279d16a8a2b0451149f46cf538ac6eb
Biftin <enderdraggon@gmail.com>
2023-04-06 12:35:22 +0200

    add comments to fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    # Dotfiles management
     alias config='/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
 end
 
+# Use starship prompt
 starship init fish | source
